8195cbbcd19dc5906c3d1e5d18b326e160401d08
michaelbittner <michaelbittner@hotmail.com>
2026-01-14 00:07:49 -0700

    uncomment Movement function calls in Test.lua

--- TurtleClearRoom/Test.lua
+++ TurtleClearRoom/Test.lua
@@ -1,7 +1,7 @@
--- local Movement = require("Movement")
+local Movement = require("Movement")
 
--- Movement.forward()
--- Movement.forward()
+Movement.forward()
+Movement.forward()
 print("hello world")
 local util = require("Utils")
 print("squaring 5 ".. util.square(5))
