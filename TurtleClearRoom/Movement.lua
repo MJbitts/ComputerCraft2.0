7606d6c5a22242ee609c57800eeea269175bb33a
michaelbittner <michaelbittner@hotmail.com>
2026-01-14 00:09:47 -0700

    move file open and close calls inside Movement.forward function

--- TurtleClearRoom/Movement.lua
+++ TurtleClearRoom/Movement.lua
@@ -1,18 +1,18 @@
 local Movement = {}
 
 local filename = "directions.txt"
-local file = fs.open(filename, "w")
 
 local turtleOrginX = 0
 local turlteOrginY = 0
 local directions = 0
 
 function Movement.forward()
+    local file = fs.open(filename, "w")
     turtle.forward()
     turtleOrginX = turtleOrginX+1;
-
+    
     file.writeLine("turtle.forward()")
     print(turtleOrginX)
+    file.close()
 end
-file.close()
 return Movement
