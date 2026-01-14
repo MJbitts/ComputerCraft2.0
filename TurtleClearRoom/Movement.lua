42b139b9d54b4fe7b211079c95bc91a9d99a846f
michaelbittner <michaelbittner@hotmail.com>
2026-01-14 00:31:44 -0700

    fix Movement module: correct variable names and enhance direction handling in forward, up, and down functions

--- TurtleClearRoom/Movement.lua
+++ TurtleClearRoom/Movement.lua
@@ -2,17 +2,42 @@ local Movement = {}
 
 local filename = "directions.txt"
 
-local turtleOrginX = 0
-local turlteOrginY = 0
-local directions = 0
+local turtleOriginX = 0
+local turtleOriginY = 0
+local turtleOriginZ = 0
+local direction = 0
 
 function Movement.forward()
     local file = fs.open(filename, "w")
     turtle.forward()
-    turtleOrginX = turtleOrginX+1;
+    if direction == 0 then -- Facing North
+        turtleOriginZ = turtleOriginZ-1;
+    elseif direction == 1 then -- Facing East
+        turtleOriginX = turtleOriginX+1
+    elseif direction == 2 then -- Facing South
+        turtleOriginZ = turtleOriginZ+1
+    else -- Facing West
+        turtleOriginX = turtleOriginX-1
+    end
+    file.writeLine(direction..":"..turtleOriginX..","..turtleOriginY..",".. turtleOriginZ)
+    file.close()
     
-    file.writeLine("turtle.forward()")
-    print(turtleOrginX)
+end
+
+function Movement.up()
+     local file = fs.open(filename, "w")
+    turtle.up()
+    turtleOriginZ = turtleOriginZ+1
+    file.writeLine(direction..":"..turtleOriginX..","..turtleOriginY..",".. turtleOriginZ)
     file.close()
 end
+
+function Movement.down()
+     local file = fs.open(filename, "w")
+    turtle.down()
+    turtleOriginZ = turtleOriginZ-1
+    file.writeLine(direction..":"..turtleOriginX..","..turtleOriginY..",".. turtleOriginZ)
+    file.close()
+end
+
 return Movement
