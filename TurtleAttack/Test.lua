3bd561ecdc842ea4de1f50c0ffbdec83dc0a41fd
michaelbittner <michaelbittner@hotmail.com>
2026-01-16 12:08:57 -0700

    fix Test.lua: enhance peripheral handling by adding return home functionality

--- TurtleAttack/Test.lua
+++ TurtleAttack/Test.lua
@@ -1,7 +1,12 @@
 tel = peripheral.find("end_automata")
 
 if tel then 
-    print("peripheral found")
+    print("peripheral found")  
+    local returnHome = tel.savePoint()
+    local e,k =os.pullEvent("key")
+    if k == keys.s then
+        print("returning home")
+    end
 else 
     print("Peripherial not found try to reninstall core")
 end
