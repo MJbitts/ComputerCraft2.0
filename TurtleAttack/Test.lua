5868a2439f60cf7bcbc7a34aa37c1f620268c8fe
michaelbittner <michaelbittner@hotmail.com>
2026-01-16 12:25:10 -0700

    fix Test.lua: reorder key event handling for improved save point functionality

--- TurtleAttack/Test.lua
+++ TurtleAttack/Test.lua
@@ -1,12 +1,18 @@
 tel = peripheral.find("end_automata")
 
 if tel then 
-    print("peripheral found")  
-    local saveHomeevent,message = tel.savePoint("savedHome")
+    print("peripheral found") 
     local e,k =os.pullEvent("key")
-    print("point saved")
-    print("Please press H tor return home")
+    if k == keys.t then
+        print("moving forward")
+        turtle.forard()
+    end
+    print("Please push h to save point")
     if k == keys.h then
+        local saveHomeevent,message = tel.savePoint("savedHome")
+        print("point saved")
+        print("Please press B tor return home")
+    elseif k == keys.b then
         print("returning home")
         warpToPoint("savedHome")
     end
