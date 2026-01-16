fda6ec7334e302885c2ceafadef7f9bd8000b17b
michaelbittner <michaelbittner@hotmail.com>
2026-01-16 12:29:17 -0700

    fix Test.lua: improve user prompts for movement and saving functionality

--- TurtleAttack/Test.lua
+++ TurtleAttack/Test.lua
@@ -1,21 +1,22 @@
 tel = peripheral.find("end_automata")
-
+while true do
 if tel then 
     print("peripheral found") 
+    print("If you  want to go foward press t, if you want to save push h")
+    print("If you want to warp back please press b ")
     local e,k =os.pullEvent("key")
     if k == keys.t then
         print("moving forward")
         turtle.forward()
     end
-    print("Please push h to save point")
     if k == keys.h then
         local saveHomeevent,message = tel.savePoint("savedHome")
         print("point saved")
-        print("Please press B tor return home")
     elseif k == keys.b then
         print("returning home")
         warpToPoint("savedHome")
     end
 else 
     print("Peripherial not found try to reninstall core")
+end
 end
