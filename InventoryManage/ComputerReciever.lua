a5ab75978ea5f30c4335e0143f8c9cb30d114757
michaelbittner <michaelbittner@hotmail.com>
2026-01-20 11:18:26 -0700

    fix(ComputerReciever): correct inventory check logic for wireless connection

--- InventoryManage/ComputerReciever.lua
+++ InventoryManage/ComputerReciever.lua
@@ -1,14 +1,18 @@
 local wireLess = peripheral.wrap("right")
 local inventory = peripheral.find("inventoryManager")
-if wireless and inventory then
-    print("found wireless connection")
-    modem.open(5)
-    event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-    print("I received a message : ".. message)
-    if senderChannel == 5 and message == ("Dump") then
-        inventory.removeItemFromPlayer("up",nil,9,26,nil)
+if wireless then
+    if peripheral then
+        print("found wireless connection")
+        modem.open(5)
+        event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+        print("I received a message : ".. message)
+        if senderChannel == 5 and message == ("Dump") then
+            inventory.removeItemFromPlayer("up",nil,9,26,nil)
+        end
+    else 
+        print("Didn't find inventory")
     end
 else
-    print("Didn't find router or inventory please reinstal")
+    print("Didn't find router")
 end
 
