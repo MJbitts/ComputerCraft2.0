71c5b1ad2e79b24522d950f6ffab7f0b86069be5
michaelbittner <michaelbittner@hotmail.com>
2026-01-20 11:39:13 -0700

    fix(DumpOnPocket): update transmit method parameters for correct data transmission

--- InventoryManage/DumpOnPocket.lua
+++ InventoryManage/DumpOnPocket.lua
@@ -2,7 +2,7 @@ local wireLess = peripheral.wrap("back")
 if wireLess then
     print("found wireless connection")
     wireLess.open(5)
-    wireLess.transmit("Dump")
+    wireLess.transmit(5,5,"Dump")
 else
     print("Didn't find router or inventory please reinstal")
 end
