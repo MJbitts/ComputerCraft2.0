25aaa7fa874b499d73a662509e063a2654fe2f06
michaelbittner <michaelbittner@hotmail.com>
2026-01-20 11:29:18 -0700

    fix(ComputerReciever): correct modem opening method to use wireLess peripheral

--- InventoryManage/ComputerReciever.lua
+++ InventoryManage/ComputerReciever.lua
@@ -3,7 +3,7 @@ local inventory = peripheral.find("inventory_manager")
 if wireLess then
     if inventory then
         print("found wireless connection")
-        modem.open(5)
+        wireLess.open(5)
         event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
         print("I received a message : ".. message)
         if senderChannel == 5 and message == ("Dump") then
