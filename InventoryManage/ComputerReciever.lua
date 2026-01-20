3d9bd3df893b1dedcd90fbdfe985f3288f4b42ed
michaelbittner <michaelbittner@hotmail.com>
2026-01-20 12:44:03 -0700

    fix(ComputerReciever): simplify item removal logic in response to 'Dump' message

--- InventoryManage/ComputerReciever.lua
+++ InventoryManage/ComputerReciever.lua
@@ -8,19 +8,8 @@ if wireLess then
             event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
             print("I received a message : ".. message)
             if senderChannel == 5 and message == ("Dump") then
-                myInv = inventory.getItems()
-                local foo = {}
-                for k,v in pairs(foo) do
-                    table.insert(foo,k,v,count)
-                end
-                for k,v in pairs(foo) do
-                    if k>=9 and k<=26 then
-                        print(k..": "..v)
-                        inventory.removeItemFromPlayer("back",v,k)
-                    end
-                end
+                inventory.removeItemFromPlayer("back", {toSlot=26, fromSlot=9})
             end
-                
         end
         term.clear()
     else 
